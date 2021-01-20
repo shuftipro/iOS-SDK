2dab0cd3de291623a369d19544210dc7b9db0093
shuftipro <support@shuftipro.com>
2021-01-20 15:28:04 +0500

    Added all verfications(verification with OCR, without OCR and restful API) in one sdk.

---
+++ Deprecated/README.md
@@ -0,0 +1,23 @@
+[![](https://raw.githubusercontent.com/shuftipro/RESTful-API-v1.2/master/assets/banner.jpg)](https://www.shuftipro.com/)
+
+# Introduction
+
+Shufti Pro has designed this Verification API document for its customers that have signed up for our next-generation service pack. This document will explain various kinds of verification services included in this service pack, how they are provided and what kind of data is required from our clients to perform these verifications successfully.
+
+**Shufti Pro’s AI & HI hybrid technology ensures 99.6% accurate results and quickest response time.**
+
+Shufti Pro’s API supports verification with and without OCR. 
+
+
+## Verification
+In order to get verified, customers will have themselves verified through their mobile phones. They will do it through the merchant's mobile application. Merchant will collect the information and send us the data for verification. The Merchant shall provide us with the proofs(Videos). We will not collect them directly from the user.
+
+    
+* ### With OCR
+In verification with OCR, it means that the merchant has not provided us proofs (images/videos) and also no data in some keys. In this verification Shufti Pro will perform extraction of data from those proofs and finally verify the data. 
+Consult [This Document](verification-with-ocr) for the Verification with OCR. 
+	
+* ### Without OCR
+In verification without OCR, merchant gives us the data in keys as well as all the proofs required then Shufti Pro just have to verify the data. No customer interaction takes place in this kind of verification.
+
+Consult [This Document](verification-without-ocr) for the Verification without OCR.
---
+++ Shufti Pro Demo/Shufti Pro Demo/ViewController.swift
@@ -0,0 +1,304 @@
+//
+//  ViewController.swift
+//  Shufti Pro Demo
+//
+//  Created by Swift Developer on 9/24/18.
+//  Copyright © 2018 Programmers Force. All rights reserved.
+//
+
+import UIKit
+import ShuftiPro
+
+class ViewController: UIViewController {
+
+    let clientIdStr = "" // your client id here
+    let secretKeyStr = "" // your secret key here
+    let accessToken = "" // your accessToken here
+    var referenceKey = ""
+    var showRatingScreen = false
+    var authKeys = Dictionary<String, String>()
+    var configs = Dictionary<String, Any>()
+
+    @IBOutlet weak var faceCheckImg: UIImageView!
+    @IBOutlet weak var documentCheckImg: UIImageView!
+    @IBOutlet weak var documentTwoCheckImg: UIImageView!
+    @IBOutlet weak var addressCheckImg: UIImageView!
+    @IBOutlet weak var consentCheckImg: UIImageView!
+    @IBOutlet weak var twoFactorCheckImg: UIImageView!
+    @IBOutlet weak var backgroundCheckImg: UIImageView!
+
+
+
+    
+    var faceVerification = false
+    var documentVerification = false
+    var documentTwoVerification = false
+    var addressVerification = false
+    var concentVerification = false
+    var twoFactorVerification = false
+    var backgroundVerification = false
+
+
+    
+    @IBOutlet weak var allowOnlineBtn: UIButton!
+    @IBOutlet weak var allowOfflineBtn: UIButton!
+    @IBOutlet weak var showPrivacyBtn: UIButton!
+    @IBOutlet weak var showConsentBtn: UIButton!
+    @IBOutlet weak var showResultBtn: UIButton!
+    @IBOutlet weak var backSideBtn: UIButton!
+    @IBOutlet weak var openWebviewBtn: UIButton!
+    @IBOutlet weak var bgServiceBtn: UIButton!
+
+
+    
+    
+    
+    var dataDictionary = Dictionary<String, Any>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+
+    }
+    
+
+    override func viewDidAppear(_ animated: Bool) {
+        
+        faceVerification = false
+        faceCheckImg.image = UIImage(named: "uncheck_radio")
+        addressVerification = false
+        documentCheckImg.image = UIImage(named: "uncheck_radio")
+        documentVerification = false
+        documentTwoCheckImg.image = UIImage(named: "uncheck_radio")
+        documentTwoVerification = false
+        addressCheckImg.image = UIImage(named: "uncheck_radio")
+        concentVerification = false
+        consentCheckImg.image = UIImage(named: "uncheck_radio")
+        twoFactorVerification = false
+        twoFactorCheckImg.image = UIImage(named: "uncheck_radio")
+        backgroundVerification = false
+        backgroundCheckImg.image = UIImage(named: "uncheck_radio")
+        
+    }
+
+
+    // Orignal button
+    @IBAction func faceVerifyBtnPressed(_ sender: Any) {
+        if faceVerification == false {
+            faceVerification = true
+            faceCheckImg.image = UIImage(named: "check_radio")
+        } else {
+            faceVerification = false
+            faceCheckImg.image = UIImage(named: "uncheck_radio")
+        }
+    }
+    
+    @IBAction func documentVerifyBtnPressed(_ sender: Any) {
+        if documentVerification == false {
+            documentVerification = true
+            documentCheckImg.image = UIImage(named: "check_radio")
+        } else {
+            documentVerification = false
+            documentCheckImg.image = UIImage(named: "uncheck_radio")
+        }
+    }
+    @IBAction func documentTwoVerifyBtnPressed(_ sender: Any) {
+        if documentTwoVerification == false {
+            documentTwoVerification = true
+            documentTwoCheckImg.image = UIImage(named: "check_radio")
+        } else {
+            documentTwoVerification = false
+            documentTwoCheckImg.image = UIImage(named: "uncheck_radio")
+        }
+    }
+    @IBAction func addressVerifyBtnPressed(_ sender: Any) {
+        if addressVerification == false {
+            addressVerification = true
+            addressCheckImg.image = UIImage(named: "check_radio")
+        } else {
+            addressVerification = false
+            addressCheckImg.image = UIImage(named: "uncheck_radio")
+        }
+    }
+    @IBAction func consentVerifyBtnPressed(_ sender: Any) {
+        if concentVerification == false {
+            concentVerification = true
+            consentCheckImg.image = UIImage(named: "check_radio")
+                        
+        } else {
+            concentVerification = false
+            consentCheckImg.image = UIImage(named: "uncheck_radio")
+
+        }
+    }
+    @IBAction func twoFactorVerifyBtnPressed(_ sender: Any) {
+        if twoFactorVerification == false {
+            twoFactorVerification = true
+            twoFactorCheckImg.image = UIImage(named: "check_radio")
+                        
+        } else {
+            twoFactorVerification = false
+            twoFactorCheckImg.image = UIImage(named: "uncheck_radio")
+
+        }
+    }
+    
+    @IBAction func backgroundChecksBtnPressed(_ sender: Any) {
+        if backgroundVerification == false {
+            backgroundVerification = true
+            backgroundCheckImg.image = UIImage(named: "check_radio")
+                        
+        } else {
+            backgroundVerification = false
+            backgroundCheckImg.image = UIImage(named: "uncheck_radio")
+
+        }
+    }
+    @IBAction func continuedBtnPressed(_ sender: Any) {
+        
+        showRatingScreen = true
+
+        if faceVerification == true || documentVerification == true || documentTwoVerification == true || addressVerification == true || concentVerification == true || twoFactorVerification == true || backgroundVerification == true{
+            referenceKey = "SDK-iOS-\(UIDevice.current.identifierForVendor!.uuidString)\(NSDate())"
+            
+        
+
+            dataDictionary = [
+                "reference": referenceKey,
+                "country": "PK",
+                "language": "EN",
+                "email": "ad@example.com",
+                "callback_url": "http://www.example.com",
+                "show_results": "1",
+                "redirect_url" : "https://www.mydummy.shuftipro.com/",
+                "show_privacy_policy": "1",
+                "show_consent": "1",
+                "verification_Mode" : "image",
+                
+                
+            ]
+            //MARK:- Ocr
+
+            if faceVerification == true {
+                dataDictionary["face"] = [
+                    "proof": ""
+                ]
+            }
+            if documentVerification == true {
+                
+                dataDictionary["document"] = [
+                    "supported_types": [
+                        "passport",
+                        "id_card",
+                        "driving_license",
+                        "credit_or_debit_card"
+                    ],
+                    "proof": "",
+                    "additional_proof" :"",
+
+                    "name": ["first_name": "",
+                              "middle_name": "",
+                              "last_name" : ""
+                    ],
+                    "backside_proof_required": "1",
+                    "dob": "",
+                    "document_number": "",
+                    "expiry_date": "",
+                    "issue_date": "",
+                    "fetch_enhanced_data": "",
+                ]
+            }
+
+            if documentTwoVerification == true  {
+                dataDictionary["document_two"] = [
+                    "supported_types": [
+                        "passport",
+                        "id_card",
+                        "driving_license",
+                        "credit_or_debit_card"
+                    ],
+                    "proof": "",
+                    "additional_proof" :"",
+
+                    "name": ["first_name": "",
+                              "middle_name": "",
+                              "last_name" : ""
+                    ],
+                    "backside_proof_required": "0",
+                    "dob": "",
+                    "document_number": "",
+                    "expiry_date": "",
+                    "issue_date": "",
+                    "fetch_enhanced_data": "",
+                    
+                ]
+            }
+        
+            
+            if addressVerification == true {
+                dataDictionary["address"] = [
+                    "full_address": "",
+                    "name": "",
+                    "backside_proof_required": "0",
+
+                    "supported_types": [
+                        "id_card",
+                        "utility_bill",
+                        "bank_statement"
+                    ],
+                ]
+            }
+            
+            if concentVerification == true {
+                dataDictionary["consent"] = [
+                    "proof": "",
+                    "text" : "text",
+                    "supported_types": [
+                        "printed"
+                  ],
+                ]
+            }
+            if twoFactorVerification == true {
+                dataDictionary["phone"] = ""
+
+            }
+            if backgroundVerification == true {
+                dataDictionary["background_checks"] = ""
+
+            }
+
+
+            authKeys = [
+                "auth_type" : "basic_auth",
+                "client_id" : clientIdStr,
+                "secret_key": secretKeyStr
+            ]
+            
+            configs = [
+                "openWebView" : "false",
+                "async" : "false"
+            ]
+            
+            let shufti = ShuftiPro()
+
+            shufti.shuftiProVerification(requestObject: dataDictionary, authKeys: authKeys, parentVC: self, configs: configs) {(result) in
+                 print("Got response from sdk: \(result)")
+        
+            }
+        } else {
+            customAlertView(titleTxt: "", messageTxt: "Please choose method of verification.")
+        }
+    }
+
+    
+    //function to show alert view
+    func customAlertView(titleTxt: String, messageTxt: String) {
+        let alertController = UIAlertController(title: titleTxt, message: messageTxt, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+ 
+}
+
